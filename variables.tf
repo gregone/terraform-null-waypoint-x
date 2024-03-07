ba97e5605491121e0b0de2ebd8ed07e2fcb1c896
gregone <gregwhat@gmail.com>
2024-03-07 17:21:10 +0100

    Add vars

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "waypoint_application" {
   description = "Name of the waypoint application, to be used in all resource names."
   type        = string
 }
+
+variable "boolean_variable" {
+  description = "A boolean variable to test the boolean type."
+  type        = bool
+}
+
+variable "number_variable" {
+  description = "A number variable to test the number type."
+  type        = number
+  
+}
